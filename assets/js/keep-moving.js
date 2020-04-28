c7b3a2edb4a8aa3d92ef67a4d852b3c03608c9bc
Matthew Phillips <matt@publicgood.studio>
2020-04-28 14:11:37 -0700

    rolling in services page and scss from the old site

---
+++ assets/js/keep-moving.js
@@ -0,0 +1,68 @@
+const keep_moving_sketch = ( sketch ) => {
+
+    let sketch_width, sketch_height;
+    let x, y, c1, c2;
+    let available_colors = [ 'f7e8f6', 'f7e8f6', 'e5b0ea', 'bd83ce' ];
+
+
+    sketch.setup = () => {
+        sketch_width = document.getElementById('canvas-container-c').clientWidth;
+        sketch_height = sketch_width * (2/3);
+        sketch.createCanvas(sketch_width, sketch_height);
+        x = 0;
+        y = sketch.random( 0, sketch_height );
+        sketch.get_next_colors();
+
+    };
+
+
+    sketch.draw = () => {
+        if ( x <= sketch_width ) {
+            let inter = sketch.map( x, 0, sketch_width, 0, 1 );
+            let c = sketch.lerpColor( c1, c2, inter );
+            sketch.stroke( c );
+            sketch.fill( c );
+
+            sketch.ellipse( x, y, 5, 5 );
+            x += 2;
+            y = sketch.random( y - 4, y + 4 );
+        } else {
+            x = 0;
+            y = sketch.random( 0, sketch_height );
+            sketch.get_next_colors();
+        }
+    }
+
+    sketch.get_next_colors = () => {
+
+        if ( !c1) {
+            c1 = sketch.color( '#' + sketch.random( available_colors ) );
+        } else {
+            c1 = c2;
+        }
+
+        c2 = sketch.color( '#' + sketch.random( available_colors ) );
+    }
+
+    sketch.windowResized = () => {
+        sketch.setDimensions();
+        sketch.resizeCanvas(sketch_width, sketch_height);
+        x = 0;
+        y = sketch.random( 0, sketch_height );
+    }
+
+    sketch.setDimensions = () => {
+
+        // We use this js file in our services page and as a fullscreen
+        // Calculate the width and height of our canvas here
+        sketch_width = document.getElementById('canvas-container-a').clientWidth;
+        if (document.getElementsByClassName('services-page')[0]) {
+            sketch_height = sketch_width * (2/3);
+        } else {
+            sketch_height = window.innerHeight;
+        }
+    }
+
+};
+
+new p5(keep_moving_sketch, 'canvas-container-a');
---
+++ assets/js/sink.js
@@ -0,0 +1,43 @@
+const sink_sketch = ( sketch ) => {
+
+    let sketch_width, sketch_height;
+
+    sketch.setup = () => {
+        sketch_width = document.getElementById('canvas-container-c').clientWidth;
+        sketch_height = sketch_width * (2/3);
+        sketch.createCanvas(sketch_width, sketch_height);
+        sketch.stroke(100);
+        sketch.frameRate(5);
+    };
+
+    sketch.draw = () => {
+        let x, y, x2, y2;
+
+        x = sketch.random(-30, sketch_width);
+        y = sketch.random(-30, sketch_height);
+        x2 = sketch.random(x-25, x+25);
+        y2 = sketch.random(y-25, y+25);
+
+        sketch.line(x, y, x2, y2);
+    }
+
+    sketch.windowResized = () => {
+        sketch.setDimensions();
+        sketch.resizeCanvas(sketch_width, sketch_height);
+    }
+
+    sketch.setDimensions = () => {
+
+        // We use this js file in our services page and as a fullscreen
+        // Calculate the width and height of our canvas here
+        sketch_width = document.getElementById('canvas-container-b').clientWidth;
+        if (document.getElementsByClassName('services-page')[0]) {
+            sketch_height = sketch_width * (2/3);
+        } else {
+            sketch_height = window.innerHeight;
+        }
+    }
+
+};
+
+new p5(sink_sketch, 'canvas-container-b');
